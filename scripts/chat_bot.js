9723f2ac319233f86a10571c8f4c2522c7e605b7
Igor <igorok.babenko@gmail.com>
2021-12-28 20:33:14 +0300

    Added dialog div area and close button

--- scripts/chat_bot.js
+++ scripts/chat_bot.js
@@ -11,6 +11,7 @@ function createChatBotIcon() {
     chatBotIcon.src = "images/chat-bot-icon.jpg"
     chatBotIcon.alt = "Chat-bot";
     chatBotIcon.classList.add("chat-bot-icon");
+    chatBotIcon.onclick = openChatBotDialog;
 
     chatBotIconDiv.appendChild(chatBotIcon);
     document.body.append(chatBotIconDiv);
@@ -27,4 +28,25 @@ function createChatBotIcon() {
     }, function() {
         $("#need-help-text").fadeOut(1000);
     });
+}
+
+function openChatBotDialog() {
+    let chatBotDialogModal = document.createElement("div");
+    chatBotDialogModal.id = "chat-bot-dialog-modal";
+    chatBotDialogModal.hidden = true;
+
+    document.body.append(chatBotDialogModal);
+
+    let closeChatBotDialogModalButton = document.createElement("span");
+    closeChatBotDialogModalButton.id = "close-chat-bot-dialog-modal-button";
+    closeChatBotDialogModalButton.onclick = closeChatBotDialog;
+    chatBotDialogModal.appendChild(closeChatBotDialogModalButton);
+
+    $("#chat-bot-icon-div").fadeOut(100);
+    $("#chat-bot-dialog-modal").fadeIn(1000);
+}
+
+function closeChatBotDialog() {
+    $("#chat-bot-dialog-modal").fadeOut(100);
+    $("#chat-bot-icon-div").fadeIn(1000);
 }
